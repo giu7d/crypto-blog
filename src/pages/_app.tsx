8e05aa325dbb768f17612639dc6ce5e76621cb54
Giuseppe Davanzo <giuseppe.davanzo@objective.com.br>
2021-08-02 17:54:40 -0300

    feat: initial project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Tailwind Studies</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
