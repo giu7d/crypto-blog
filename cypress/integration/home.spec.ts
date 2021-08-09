5177a0698134b40fc7021938f66425464b7f154f
Giuseppe Davanzo <giuseppe.davanzo@objective.com.br>
2021-08-08 23:54:21 -0300

    test(integration): add interception

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,8 +1,40 @@
+const filters = [
+  {
+    key: '0',
+    title: '0'
+  },
+  {
+    key: '1',
+    title: '1'
+  },
+  {
+    key: '2',
+    title: '2'
+  }
+]
+
+const articles = [
+  {
+    id: '0',
+    title: 'Lorem ipsum dolor',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis doloremque inventore eum quaerat saepe, corrupti non quibusdam!',
+    date: new Date(),
+    category: '1'
+  }
+]
+
 describe('Home page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/filters', { body: { filters } })
+    cy.intercept('GET', /\/api\/articles*/, { body: { articles: [] } })
+    cy.intercept('GET', '/api/articles?filter=1', { body: { articles } })
+  })
+
   it('should filter articles', () => {
     cy.visit(`/`)
       .then(() => cy.get('[data-cy=filter]'))
-      .then(() => cy.contains('Tutorial'))
+      .then(() => cy.contains(filters[1].title))
       .click()
       .then(() => cy.get('[data-cy=article]'))
       .should('have.length', 1)
